3f77398e40d6a9ef16b46b58eadb12e44ff03f1e
chandurajoli125-cyber <chandurajoli125@gmail.com>
2026-05-17 20:52:42 +0530

    Add files via upload

---
+++ lc.py
@@ -0,0 +1,26 @@
+#1st program
+print("Hello world")
+
+#2nd program
+a = int(input("enter a first number:"))
+b = int(input("enter a second number:"))
+sum=a+b
+print("Sum:",sum)
+
+#3rd program
+a = int(input("enter a first number:"))
+b = int(input("enter a second number:"))
+result = a-b
+print("subtraction:",result)
+
+#4th program
+a = int(input("enter a first number:"))
+b = int(input("enter a second number:"))
+result =a*b
+print("multiplication:",result)
+
+#5th program
+a = int(input("enter a first number:"))
+b = int(input("enter a second number:"))
+result =a/b
+print("division:",result)
