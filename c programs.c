3586549f5fa05af868f1b6df347006e4edf471e6
chandurajoli125-cyber <chandurajoli125@gmail.com>
2026-05-28 11:49:34 +0530

    Add files via upload

---
+++ c programs.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter first number:");
+	scanf("%d",&a);
+	printf("enter second number:");
+	scanf("%d",&b);
+	printf("enter third number:");
+	scanf("%d",&c);
+	printf("Addition = %d\n",a+b+c);
+	printf("subtraction = %d\n",a-b-c);
+	printf("multiplication = %d\n",a*b*c);
+	printf("division = %d\n",a/b/c);
+	return 0;
+}
